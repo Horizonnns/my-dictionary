b28ac3daf0c19723677198261e2fe20b57a48e30
mke <for.office.gthb@gmail.com>
2025-05-16 10:13:40 +0500

    Added state for search, improved style and added a button to add a word

--- src/app/main/Header.tsx
+++ src/app/main/Header.tsx
@@ -1,16 +1,34 @@
-const Header = () => {
-  return (
-    <header>
-      <h1>Header</h1>
+import { useState } from "react";
 
-      <nav>
-        <label htmlFor="search">Search</label>
-        <input
-          type="text"
-          id="search"
-          className="border-2 border-gray-300 rounded-md p-2"
-        />
+interface HeaderProps {
+  addWord: () => void;
+}
+
+const Header = (props: HeaderProps) => {
+  const [search, setSearch] = useState<string>("");
+
+  return (
+    <header className="flex justify-between items-center space-x-6 bg-gray-100 p-4 rounded-lg">
+      <h1 className="text-2xl font-bold">MyDictionary</h1>
+
+      <nav className="w-full">
+        <label htmlFor="search">
+          <input
+            type="text"
+            id="search"
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}
+            className="w-full border-2 border-gray-300 rounded-full p-1"
+          />
+        </label>
       </nav>
+
+      <button
+        className="bg-blue-500 hover:bg-blue-600 active:bg-blue-700 cursor-pointer transition-colors duration-200 text-white text-[15px] px-2.5 py-1.5 rounded-full"
+        onClick={props.addWord}
+      >
+        Добавить
+      </button>
     </header>
   );
 };
